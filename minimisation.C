3a25779360189c004308b412056d693c1ecd5ec3
Olivia <oliviavidalvelazquez@gmail.com>
2023-01-24 18:27:32 +0100

    Nettoyage et code commenté

--- minimisation.C
+++ minimisation.C
@@ -1,45 +1,70 @@
-//#include <vector>
-
-
 {
-
 	gRandom = new TRandom3(2);
+    
+    // Variables signal/bruit
+	double nb = 10000; //nombre d'évenements bruit
+    double ns = 100; //nombre d'évenements signal
+    double ntot = ns+nb; //nombre d'évenements totales
+    double k = ns/ntot; //paramètre signal sur bruit. Pour nous 0.009, on cherche s'approcher à ça
+    
+    // Variables Gauss et exponentielle pour créer signal et bruit et histogrames
+    double b_slope = 1.0; // pente exponentielle (bruit)
+    double moy = 3.0; // moyenne gaussienne (signal)
+    double sig = 0.2; // déviation standard gaussienne (signal) 
+    double amp = 5.0; // amplitude gaussienne (signal)
+    int bins = 100; // Nombre de bins 
+    double widthbin; // La largeur des bins, on la calcule après.
+    
+    // Variables pour les boucles 
+    double ki; // Pour faire l'itération sur les différents k
+    int bin; // Pour faire l'itération sur les bins
+    
+    
+    // Variables nécésaires pour calculer poisson:
+    double x; // Pour calculer le x au milieu du bin où on va évaluer la fonction f(x)
+    double val_att; // Pour calculer la valeur attendu, i.e. f(x). (lamda)
+    double val_mesure; // Pour calculer la valeur mesurée i.e. (n) 
+    
+    double poisson; // Fonction de poisson
+    double tot_poisson; // Somme des logaritmes de toutes les fonctions poissons de chaque bin
 
-	double nb = 10000;
-    double ns = 100;
-    double b_slope = 1.0;
-    double moy = 3.0;
-    double sig = 0.2;
-    double amp = 5.0;
-    double ntot = ns+nb;
-    double k = ns/ntot;
-    double likelihood[100];
-    //vector<double> Poisson(100);    
-
+    
+    // Tableaux pour le plot
+    double likelihood[100]; //likelihood des poisssons, axe y. Il faudrait voir la taille, même taille qu'iterations sur k.
+    // Il manque un tableau de k pour l'axe x. 
+    int i = 0; // Pour remplir chaque tableau pour le plot
+    
+    
+    // Fonction du bruit : 
 	TF1* fb = new TF1("background","[0]*exp(-[1]*x)",0,10);
 	fb->SetParameters(1,b_slope);
 	
+	// Fonction du signal : 
 	TF1* fs = new TF1("signal", "gaus", 0,10);
 	fs->SetParameters(amp,moy,sig);
 	
-	
-
-	TH1F* hb = new TH1F("histb","histb",100,0,10);
+	// Histogramme du bruit :
+	TH1F* hb = new TH1F("histb","histb",bins,0,10);
 	hb->FillRandom("background",nb);
 	hb->SetLineColor(kBlue);
 	
-	
-	TH1F* hs = new TH1F("hists","hists",100,0,10);
+	// Histogramme du signal :
+	TH1F* hs = new TH1F("hists","hists",bins,0,10);
 	hs->FillRandom("signal",ns);
 	hs->SetLineColor(kBlue);
     
-    TH1F* htot = new TH1F("histot","histot",100,0,10);
+    // Les deux histogramme ensemble :
+    TH1F* htot = new TH1F("histot","histot",bins,0,10);
     htot->Add(hs,hb);
+    widthbin = htot->GetBinWidth(1); // On calcule la largeur des bins pour plus tard (les boucles) tous les bins sont pareils donc on choisi par exemple de mesurer la largeur du premier.
     
-    htot->SetTitle("Histogram title");
-    htot->GetXaxis()->SetTitle("X axis title");
-    htot->GetYaxis()->SetTitle("Y axis title");
+    // Le plot avec les titres des axes:
+    
+    htot->SetTitle("Signal sur bruit");
+    htot->GetXaxis()->SetTitle("Temps"); // Pas sûre que ça soit temps xd
+    htot->GetYaxis()->SetTitle("Nombre d'évènements");
 
+    // Création de Canvas pour faire le plot, je n'ai aucune idée de comment ça marche, mais ce sont comme des figures.
     
 	TCanvas* c = new TCanvas;
 	c->cd(1);
@@ -50,59 +75,41 @@
 	htot->Write();
 	fout.Close();
 
-
-    
-    
+ 
+    // Fonction f pour les valeurs attendues.
     TF1* f = new TF1("likelihood","[0]*([1]*gaus(2)+(1-[1])*[5]*exp(-[6]*x))",0,10);
 
-    
-    double ki = 0;
-    double x = 0;
-    double val_att;
-    double widthbin;
-    widthbin = htot->GetBinWidth(1);
-    int binmin=0;
-    int binmax=99;
-    int bin;
-    double poisson;
-    int i = 0;
-    //double mult_poisson=1.;
-    
+   
+    // Boucle pour chercher le maximum de vraisemblance:
+    // Il faut chercher pour plusieurs k laquelle donne une probabilité la plus grande. On fait la probabilité pour tous les bins et on multiplie pour avoir la probabilité totale pour chaque k.
     
     for(ki=0;ki<=0.02;ki=ki+0.005){
         f->SetParameters(ntot*widthbin,ki,amp,moy,sig,1,b_slope);
-        //TH1F* hp = new TH1F("histpois","histpois",100,0,10);
-        double mult_poisson=1.;
-        for (bin = binmin; bin<=binmax; bin=bin+1){
+        tot_poisson=0.; // On défini ici à 0 pour réinisializer à chaque fois qu'on fait un boucle sur k.
+        for (bin = 0; bin<=bins-1; bin=bin+1){
             //bin=bin+1;
             x = htot->GetBinCenter(bin);
             val_att = f->Eval(x);
-            double_t binContent = htot->GetBinContent(bin);
-	        poisson = TMath::Poisson(binContent,val_att);
-	        
-            mult_poisson=mult_poisson*poisson;
-            //Poisson[bin] = fp;
-             
+            // Pour la valeur mesurée on prend la valeur du bin, combien de comptages il y a par bin.
+            val_mesure = htot->GetBinContent(bin+1); //GetBinContent a minimum 1 bin, donc il peut pas avoir 0 comme argument.
+	       
+	        // Une fois on a la valeur attendue et la valeur mesuré on calcule avec Poisson la probabilité de trouver la valeur attendue en ayant la valeur mesurée P(mesurée,attendue):
+	        poisson = TMath::Poisson(val_mesure,val_att);
+	        // On fait le log : 
+	        poisson = TMath::Log(poisson);
+	        // Comme on a fait le log on ne multiplie pas on somme:
+            tot_poisson += poisson;
             }
-            likelihood[i] = mult_poisson;
-            i = i+1 ;
-            
-            //hp->Draw();
-            //multiplications
-            //garder valeurs
+            // On veut le -log du coup on stocke avec un - :
+            likelihood[i] = - tot_poisson; //axe y, il faut aussi l'axe x pour le plot. L'axe x ce sont les k.
+            i = i+1 ; // On actualise la valeur de i pour bouger dans le vecteur likelihood
+            // Ça c'est juste un print:
+            // std::cout<<likelihood[i]<<tot_poisson<<poisson<<std::endl;
+
         }
-    // plot pour chercher le minimum de k
     
-    
-    
-    //f->Eval(1);
-    //f->Draw();
-    //s(double x){
-    //for (i=0; i<=10 ; i = i+0.01) //start,stop,pas (si 100 bin en 10 x : 0.1)
-    
-    //}
-    
-    //void b(double x)
+    //std::cout<<likelihood[4]<<std::endl;    
+    // plot pour chercher le minimum de likelihood vs k, i.e. le meilleur k.
     
 } 
     
